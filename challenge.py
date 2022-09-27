7bd0c0cc7c44420906b0fa68c207289d0bca4aab
Fran Cambiagno <francambiagno@hotmail.com>
2022-09-27 20:05:32 -0300

    Funcion obtener CSV completa y funcion obtenerDirecciones completa

--- challenge.py
+++ challenge.py
@@ -1,20 +1,44 @@
 from bs4 import BeautifulSoup
 import csv
 import requests
-import ast
+from decouple import config
+import os
+from datetime import date
 
-def ObtenerCSV():
-    url = PrepararURL('https://docs.google.com/spreadsheets/d/1o8QeMOKWm4VeZ9VecgnL8BWaOlX5kdCDkXoAph37sQM/edit#gid=1691373423')
+def ObtenerCSV(urls):
+    urls = list(map(PrepararURL, urls))
+    
+    for i, url in enumerate(urls):
+        if i == 0:
+            tipos_datos = "museos"
+        elif i == 1:
+            tipos_datos = "cines"
+        else:
+            tipos_datos = "bibliotecas"
+        
+        direccion = obtenerDireccion(tipos_datos)
+        os.makedirs(direccion[0], exist_ok=True)
 
-    html = requests.get(url).text
-    soup = BeautifulSoup(html, "lxml")
-    tables = soup.find_all("table")
-    index = 0
-    for table in tables:
-        with open(str(index) + ".csv", "w", encoding='utf8') as f:
-            wr = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC, lineterminator='\n')
-            wr.writerows([[td.text for td in row.find_all("td")] for row in table.find_all("tr")])
-        index = index + 1
+        html = requests.get(url).text
+        soup = BeautifulSoup(html, "lxml")
+        tables = soup.find_all("table")
+        for table in tables:
+            with open(direccion[0] + direccion[1] + ".csv", "w", encoding='utf8') as f:
+                wr = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC, lineterminator='\n')
+                wr.writerows([[td.text for td in row.find_all("td")] for row in table.find_all("tr")])
+
+def obtenerDireccion(datos):
+    """
+    Genera la direccion donde se va guardar y el nombre del archivo csv que obtengamos. 
+    """
+
+    hoy = date.today()
+    meses = ("enero", "febrero", "marzo", "abri", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre")
+    dia = hoy.day
+    mes = hoy.month
+    anio = hoy.year
+    return   (f'{datos}/{anio}-{meses[mes-1]}', f'/{datos}-{dia}-{mes}-{anio}')
+
 
 def PrepararURL(url):
     """
@@ -32,7 +56,10 @@ def run():
     """
     Es el cuerpo del programa
     """
-    ObtenerCSV()
+    urls = [config('URL_MUSEOS'),
+            config('URL_CINES'),
+            config('URL_BIBLIOTECAS')]
+    ObtenerCSV(urls)
 
 
 if __name__ == "__main__":
